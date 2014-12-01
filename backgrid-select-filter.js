4d2f63a966386cff423aba267d1fccb7e521d65d
mmxkz <mmxkz@ya.ru>
2014-12-01 14:52:21 +0500

    Update backgrid-select-filter.js

--- backgrid-select-filter.js
+++ backgrid-select-filter.js
@@ -1,53 +1,31 @@
-/*
-  Backgrid select-filter extension
-  http://github.com/amiliaapp/backgrid-select-filter
-
-  Copyright (c) 2014 Amilia Inc.
-  Written by Martin Drapeau
-  Licensed under the MIT @license
- */
 (function(){
   var SelectFilter = Backgrid.Extension.SelectFilter = Backbone.View.extend({
-    tagName: "select",
-    className: "backgrid-filter",
+    tagName: "div",
+    className: "menu backgrid-filter",
     template: _.template([
       "<% for (var i=0; i < options.length; i++) { %>",
-      "  <option value='<%=JSON.stringify(options[i].value)%>' <%=options[i].value === initialValue ? 'selected=\"selected\"' : ''%>><%=options[i].label%></option>",
+      "  <option class=\"item\" value=<%=JSON.stringify(options[i].value)%>><%=options[i].label%></option>",
       "<% } %>"
     ].join("\n")),
     events: {
-      "change": "onChange"
-    },
-    defaults: {
-      selectOptions: undefined,
-      field: undefined,
-      clearValue: null,
-      initialValue: undefined,
-      makeMatcher: function(value) {
-        return function(model) {
-          return model.get(this.field) == value;
-        };
-      }
+      "click": "onChange"
     },
     initialize: function(options) {
       SelectFilter.__super__.initialize.apply(this, arguments);
-
-      _.defaults(this, options || {}, this.defaults);
-      if (_.isEmpty(this.selectOptions) || !_.isArray(this.selectOptions)) throw "Invalid or missing selectOptions.";
-      if (_.isEmpty(this.field) || !this.field.length) throw "Invalid or missing field.";
-      if (this.initialValue === undefined) this.initialValue = this.clearValue;
-
+      console.log(options.selectOptions);
+      if (!options.selectOptions || !_.isArray(options.selectOptions)) throw "Invalid or missing selectOptions.";
+      if (!options.field || !options.field.length) throw "Invalid or missing field.";
+      this.selectOptions = options.selectOptions;
+      this.field = options.field;
+      this.clearValue = options.clearValue !== undefined ? options.clearValue : null;
+      this.initialValue = options.initialValue !== undefined ? options.initialValue : this.clearValue;
       var collection = this.collection = this.collection.fullCollection || this.collection;
       var shadowCollection = this.shadowCollection = collection.clone();
-
       this.listenTo(collection, "add", function (model, collection, options) {
         shadowCollection.add(model, options);
       });
       this.listenTo(collection, "remove", function (model, collection, options) {
         shadowCollection.remove(model, options);
-      });
-      this.listenTo(collection, "sort", function (col) {
-        if (this.currentValue() == this.clearValue) shadowCollection.reset(col.models);
       });
       this.listenTo(collection, "reset", function (col, options) {
         options = _.extend({reindex: true}, options || {});
@@ -57,24 +35,19 @@
       });
     },
     render: function() {
-      this.$el.empty().append(this.template({
+      this.$el.append(this.template({
         options: this.selectOptions,
         initialValue: this.initialValue
       }));
-			this.onChange();
       return this;
-    },
-    currentValue: function() {
-      return JSON.parse(this.$el.val());
     },
     onChange: function(e) {
       var col = this.collection,
         field = this.field,
-        value = this.currentValue(),
-        matcher = _.bind(this.makeMatcher(value), this);
-
-      if (col.pageableCollection)
-        col.pageableCollection.getFirstPage({silent: true});
+        value = JSON.parse(e.target.value),
+        matcher = function(model) {
+          return model.get(field) == value;
+        };
 
       if (value !== this.clearValue)
         col.reset(this.shadowCollection.filter(matcher), {reindex: false});
